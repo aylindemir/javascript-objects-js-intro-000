1354c36ee160bd8975b71f949be805fd7743a873
Aylin Demir <aylin.de@outlook.com>
2019-12-26 19:30:50 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist={};
+playlist.billieEllish = 'I love you'
+
+function updatePlaylist(object,artist,song){
+  return object[artist] = song
+}
+
+function removeFromPlaylist(object,artist){
+Delete object.artist
+return object
+}
